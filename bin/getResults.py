28753f747a812e3db936658a8c9c04c8bb61ce84
TekuDruida <TekuDruida@github.com>
2017-03-22 19:36:25 +0100

    getResults 2.0

--- bin/getResults.py
+++ bin/getResults.py
@@ -1,3 +1,5 @@
+import sys
+
 def printTable(table):
 	print("      0\t\t|\t1\t|\t2\t|\t3\t|\t4\t|\t5\t|\t")
 	print("-------------------------------------------------------------------------------------------------")
@@ -48,54 +50,48 @@ def sumColums(table):
 	return suma
 
 
-files = ("1000-100k-0-0.01", "1000-100k-0-0.1", "1000-150k-0-0.01"
-	, "100-100k-0-0.1", "100-100k-1-10", "100-150k-0-0.01"
-	, "10-100k-0-0.01", "10-100k-0-0.1", "10-100k-1-0.01"
-	, "10-100k-1-0.1", "10-100k-1-100", "10-100k-1-10"
-	, "10-100k-1-1", "10-150k-0-0.01", "10-150k-1-100", "10-150k-1-10")
+fname = sys.argv[1]
 
+print(fname+"\n")
+file = open("tfg/outputs/comparativas/"+fname+".txt",'r')
+table = [[0 for x in range(7)] for y in range(7)]
 
-for f in files:
-	print(f+"\n")
-	file = open("../outputs/comparativa-"+f+".txt",'r')
-	table = [[0 for x in range(7)] for y in range(7)]
-
+line = file.readline()
+while(line != ""):
+	esp = line.split('\t')[0]
+	pred = line.split('\t')[1]
+	#print(esp+" "+pred)
+	table[int(pred)][int(esp)] += 1
 	line = file.readline()
-	while(line != ""):
-		esp = line.split('\t')[0]
-		pred = line.split('\t')[1]
-		#print(esp+" "+pred)
-		table[int(pred)][int(esp)] += 1
-		line = file.readline()
-	file.close()
-	addTotals(table)
-	printTable(table)
+file.close()
+addTotals(table)
+printTable(table)
 
-	#print(sumColum(2,table))
-	#print(sumRow(5,table))
+#print(sumColum(2,table))
+#print(sumRow(5,table))
 
-	for i in range(0,6):
-		try:
-			print("Class: "+str(i))
-			print("")
-			p = table[i][i]/table[i][6]*100
-			print("Precision = "+str(p)+" ("+str(table[i][i])+"/"+str(table[i][6])+")")
-			r = table[i][i]/table[6][i]*100
-			print("Recall = "+str(r)+" ("+str(table[i][i])+"/"+str(table[6][i])+")")
-			f1 = (2*p*r)/(p+r)
-			print("F1 = "+str(f1)+" ("+str(2*p*r)+"/"+str(p+r)+")")
-		except Exception as e:
-			print("Esta clase nunca se ha predecido")
-		
-		print("---------------------")
-	print("Global estadistics:")
-	pg = sumDiagonal(table)/sumRows(table)*100
-	print("Precision = "+str(pg)+" ("+str(sumDiagonal(table))+"/"+str(sumRows(table))+")")
-	rg = sumDiagonal(table)/sumColums(table)*100
-	print("Recall = "+str(rg)+" ("+str(sumDiagonal(table))+"/"+str(sumColums(table))+")")
-	f1g = (2*pg*rg)/(pg+rg)
-	print("F1 = "+str(f1g)+" ("+str(2*pg*rg)+"/"+str(pg+rg)+")")
-
+for i in range(0,6):
+	try:
+		print("Class: "+str(i))
+		print("")
+		p = table[i][i]/table[i][6]*100
+		print("Precision = "+str(p)+" ("+str(table[i][i])+"/"+str(table[i][6])+")")
+		r = table[i][i]/table[6][i]*100
+		print("Recall = "+str(r)+" ("+str(table[i][i])+"/"+str(table[6][i])+")")
+		f1 = (2*p*r)/(p+r)
+		print("F1 = "+str(f1)+" ("+str(2*p*r)+"/"+str(p+r)+")")
+	except Exception as e:
+		print("Esta clase nunca se ha ")
+	
 	print("---------------------")
-	print("resum "+f+" "+str(pg)+" "+str(rg)+" "+str(f1g))
-	print("---------------------")
+print("Global estadistics:")
+pg = sumDiagonal(table)/sumRows(table)*100
+print("Precision = "+str(pg)+" ("+str(sumDiagonal(table))+"/"+str(sumRows(table))+")")
+rg = sumDiagonal(table)/sumColums(table)*100
+print("Recall = "+str(rg)+" ("+str(sumDiagonal(table))+"/"+str(sumColums(table))+")")
+f1g = (2*pg*rg)/(pg+rg)
+print("F1 = "+str(f1g)+" ("+str(2*pg*rg)+"/"+str(pg+rg)+")")
+
+print("---------------------")
+print("resum "+f+" "+str(pg)+" "+str(rg)+" "+str(f1g))
+print("---------------------")
