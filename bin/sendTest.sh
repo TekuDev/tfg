720dc31193811cb6b8074530454e3f6c5f250046
TekuDruida <TekuDruida@github.com>
2017-03-23 20:49:37 +0100

    version python

--- bin/sendTest.sh
+++ bin/sendTest.sh
@@ -5,4 +5,4 @@ tfg/libsvm-3.21/svm-predict tfg/inputs/featuresCoded/fcDev$3.txt tfg/outputs/mod
 
 cut -d' ' -f1 tfg/inputs/featuresCoded/fcDev5.txt | paste - tfg/outputs/predictions/$3-$1-$2_predict.txt > tfg/outputs/comparativas/c_$3-$1-$2.txt
 
-python3 tfg/bin/getResults.py c_$3-$1-$2 > tfg/outputs/results/r_$3-$1-$2.txt
+python3.2 tfg/bin/getResults.py c_$3-$1-$2 > tfg/outputs/results/r_$3-$1-$2.txt
