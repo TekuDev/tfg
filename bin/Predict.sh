2e4218b86cd60d03f8fa26c61ba7507d4546a5cc
TekuDruida <TekuDruida@github.com>
2017-04-10 18:55:49 +0200

    predictions.sh

---
+++ bin/Predict.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cortes=(5 10 50)
+kernels=(0 1)
+Cs=(0.01 0.1 1 10 100)
+nNones=(150)
+for i in ${kernels[@]};
+do
+	for j in ${Cs[@]};
+	do
+		for t in ${cortes[@]};
+		do
+			for h in ${nNones[@]};
+			do
+				qsub tfg/bin/sendPred.sh ${i} ${j} ${t} ${h}
+			done
+		done
+	done
+done
--- bin/sendPred.sh
+++ bin/sendPred.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-tfg/libsvm-3.21/svm-predict tfg/inputs/featuresCoded/fcDev$3.txt tfg/outputs/models/train$3-$1-$2.model tfg/outputs/predictions/$3-$1-$2_predict.txt > tfg/outputs/accuracy/$3-$1-$2_acc.txt
+tfg/libsvm-3.21/svm-predict tfg/inputs/featuresCoded/fcDev$3.txt tfg/outputs/models/train$3-$4-$1-$2.model tfg/outputs/predictions/$3-$4-$1-$2_predict.txt > tfg/outputs/accuracy/$3-$4-$1-$2_acc.txt
+cut -d' ' -f1 tfg/inputs/featuresCoded/fcDev5.txt | paste - tfg/outputs/predictions/$3-$4-$1-$2_predict.txt > tfg/outputs/comparativas/c_$3-$4-$1-$2.txt
+
+python3.2 tfg/bin/getResults.py c_$3-$4-$1-$2 > tfg/outputs/results/r_$3-$4-$1-$2.txt
