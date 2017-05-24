6f9cc829175c1b383d593c8db51d136be5d52998
TekuDruida <TekuDruida@github.com>
2017-05-24 15:30:47 +0200

    script para nueva prueba

---
+++ bin/getOutt0.py
@@ -0,0 +1,19 @@
+import string
+import sys
+import random
+
+readFile = sys.argv[1]
+writeFile = sys.argv[2]
+
+
+old = open(readFile,'r')
+new = open(writeFile,'w')
+
+line = old.readline()
+while(line != ''):
+	if line.find("w1=t0") == -1:
+		new.write(line)
+	line = old.readline()
+
+old.close()
+new.close()
